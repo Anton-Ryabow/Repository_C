79ae01367e8b62df7efa204b3bca7bf50b5ec656
Anton Ryabov <Sapog-skorohod@mail.ru>
2024-03-03 16:30:54 +0400

    Второй вывод

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(0, "RUS");
     printf("Hello world!\n");
+    printf("Первое добавление");
     return 0;
 }
